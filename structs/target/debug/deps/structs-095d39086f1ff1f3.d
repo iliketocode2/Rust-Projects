1141a82958504154352fe408b3e6bb845fc0cbde
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2024-12-29 14:17:30 -0500

    5.1

---
+++ structs/target/debug/deps/structs-095d39086f1ff1f3.d
@@ -0,0 +1,5 @@
+C:\Users\William\OneDrive\Desktop\VS Code PROJECTS\Rust-Projects\structs\target\debug\deps\structs-095d39086f1ff1f3.exe: src/main.rs
+
+C:\Users\William\OneDrive\Desktop\VS Code PROJECTS\Rust-Projects\structs\target\debug\deps\structs-095d39086f1ff1f3.d: src/main.rs
+
+src/main.rs:

53658907b1311fed857e89660cb0125710474a37
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2024-12-29 10:48:46 -0500

    4.1

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+C:\Users\William\OneDrive\Desktop\VS Code PROJECTS\Rust-Projects\ownership\target\debug\deps\ownership.exe: src/main.rs
+
+C:\Users\William\OneDrive\Desktop\VS Code PROJECTS\Rust-Projects\ownership\target\debug\deps\ownership.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\William\OneDrive\Desktop\VS\ Code\ PROJECTS\Rust-Projects\ownership\target\debug\ownership.exe: C:\Users\William\OneDrive\Desktop\VS\ Code\ PROJECTS\Rust-Projects\ownership\src\main.rs

759d484fbc3f7440d2deae1f37cabf7e07d576c4
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2024-12-29 23:06:32 -0500

    5.3

---
+++ structs_ex/target/debug/deps/structs_ex-82e114fccba5c298.d
@@ -0,0 +1,5 @@
+C:\Users\William\OneDrive\Desktop\VS Code PROJECTS\Rust-Projects\structs_ex\target\debug\deps\structs_ex-82e114fccba5c298.exe: src/main.rs
+
+C:\Users\William\OneDrive\Desktop\VS Code PROJECTS\Rust-Projects\structs_ex\target\debug\deps\structs_ex-82e114fccba5c298.d: src/main.rs
+
+src/main.rs:
